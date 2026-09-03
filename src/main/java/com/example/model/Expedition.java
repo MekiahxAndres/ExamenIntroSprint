ae8491970bb6e64dcc3e0dde550c7a9d07b07a33
JuanJaramill06 <1109665552@u.icesi.edu.co>
2026-09-03 14:26:12 -0500

    marzo

---
+++ src/main/java/com/example/model/Expedition.java
@@ -0,0 +1,98 @@
+package com.example.model;
+
+public class Expedition {
+
+    private int id;
+    private String name;
+    private String Code;
+    private String region;
+    private String baseCamp;
+    private String leader;
+    private String endDate;
+    private String startDate;
+    private String state;
+
+    public Expedition(int id, String name, String code, String region, String baseCamp, String leader, String endDate, String startDate, String state) {
+        this.id = id;
+        this.name = name;
+        Code = code;
+        this.region = region;
+        this.baseCamp = baseCamp;
+        this.leader = leader;
+        this.endDate = endDate;
+        this.startDate = startDate;
+        this.state = state;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return Code;
+    }
+
+    public void setCode(String code) {
+        Code = code;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getBaseCamp() {
+        return baseCamp;
+    }
+
+    public void setBaseCamp(String baseCamp) {
+        this.baseCamp = baseCamp;
+    }
+
+    public String getLeader() {
+        return leader;
+    }
+
+    public void setLeader(String leader) {
+        this.leader = leader;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
---
+++ src/main/java/com/example/model/Sighting.java
@@ -0,0 +1,109 @@
+package com.example.model;
+
+public class Sighting {
+
+    private int id;
+    private String sightingId;
+    private String name;
+    private String description;
+    private String scientificName;
+    private String sightingAt;
+    private String location;
+    private int quantity;
+    private String confidenceLevel;
+    private int expeditionId;
+
+    public Sighting(int id, String sightingId, String name, String description, String scientificName, String sightingAt, String location, int quantity, String confidenceLevel, int expeditionId) {
+        this.id = id;
+        this.sightingId = sightingId;
+        this.name = name;
+        this.description = description;
+        this.scientificName = scientificName;
+        this.sightingAt = sightingAt;
+        this.location = location;
+        this.quantity = quantity;
+        this.confidenceLevel = confidenceLevel;
+        this.expeditionId = expeditionId;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getExpeditionId() {
+        return expeditionId;
+    }
+
+    public void setExpeditionId(int expeditionId) {
+        this.expeditionId = expeditionId;
+    }
+
+    public String getConfidenceLevel() {
+        return confidenceLevel;
+    }
+
+    public void setConfidenceLevel(String confidenceLevel) {
+        this.confidenceLevel = confidenceLevel;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getSightingAt() {
+        return sightingAt;
+    }
+
+    public void setSightingAt(String sightingAt) {
+        this.sightingAt = sightingAt;
+    }
+
+    public String getScientificName() {
+        return scientificName;
+    }
+
+    public void setScientificName(String scientificName) {
+        this.scientificName = scientificName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSightingId() {
+        return sightingId;
+    }
+
+    public void setSightingId(String sightingId) {
+        this.sightingId = sightingId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
